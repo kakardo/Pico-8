17191e369bf3bae9df98229ca5cf19623fd8512a
Kardo Rostam <kardo.rostam@gmail.com>
2025-07-20 04:43:34 +0200

    Did some test functions with prints to terminal

--- main_tester.lua
+++ main_tester.lua
@@ -17,17 +17,47 @@ tick_count = 0
 score = 0
 
 ---------------------------------------------------------------
+total_tests = 0
+successful_tests = 0
+failed_tests = 0
 
-function tester_tables()
+has_run = false
 
+function boolean_test(boolean_argument)
+	total_tests += 1
 
+	if boolean_argument then
+		successful_tests += 1
+		printh("Success")
+	else
+		failed_tests += 1
+		printh("Fail")
+	end
 end
 
+function print_test_stats()
+	percentage = successful_tests/total_tests*100
+	printh("Results = "..percentage.."%"..
+		" ("..successful_tests.." SUCCESS / "..failed_tests.." FAIL)")
+end
+
+function tester_tables()
+	printh("Table Tester")
+	table = {"Crash","Spyro","Ratchet","Clank","Jak","Daxter",}
+
+	boolean_test(#table == 6)
+	print_test_stats()
+end
+
+
 function _init()
-	store_empty_cells()
-	init_head()
-	init_body()
-	init_fruit()
+	--store_empty_cells()
+	--init_head()
+	--init_body()
+	--init_fruit()
+	printh("TESTER START")
+	tester_tables()
+	printh("Run time: "..time().."\n")
 end
 
 function _update()
