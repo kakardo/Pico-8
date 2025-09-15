1884eda4504c16ddcac5f800310a5660a1229e9e
Kardo Rostam <kardo.rostam@gmail.com>
2025-09-15 23:27:25 +0200

    Main has been made for game 2

---
+++ game carts/02 platonic solids viewer/main.lua
@@ -0,0 +1,17 @@
+--[[ MAIN.LUA
+	- Main loop of the program.
+	@author Kardo Rostam
+	@date 2025-09-15
+]]--
+
+function _init()
+end
+
+function _update()
+end
+
+function _draw()
+cls()
+end
+
+-- LAST_LINE_MAIN_LUA
