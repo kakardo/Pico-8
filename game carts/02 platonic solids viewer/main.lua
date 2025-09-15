a3717b895c304dcb6eff4767e48e5510957999db
Kardo Rostam <kardo.rostam@gmail.com>
2025-09-15 23:44:37 +0200

    Research and goals for projects

--- game carts/02 platonic solids viewer/main.lua
+++ game carts/02 platonic solids viewer/main.lua
@@ -2,6 +2,22 @@
 	- Main loop of the program.
 	@author Kardo Rostam
 	@date 2025-09-15
+		
+	https://en.wikipedia.org/wiki/Platonic_solid
+	https://en.wikipedia.org/wiki/3D_projection
+	https://en.wikipedia.org/wiki/Orthographic_projection
+	https://en.wikipedia.org/wiki/Perspective_(graphical)
+
+	TODO
+	- Implement the five platonic solids:
+		Regular tetrahedron
+		Cube
+		Regular octahedron 
+		Regular dodecahedron
+		Regular icosahedron 
+	- Implement two perspective:
+		Orthographic
+		Perspective
 ]]--
 
 function _init()
