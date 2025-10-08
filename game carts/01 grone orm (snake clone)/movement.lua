4d67c8874bf64895c17f57c932a3f76d7eb47e20
Kardo Rostam <kardo.rostam@gmail.com>
2025-10-08 22:15:48 +0200

    Moved check_if_safe() to movement.lua

--- game carts/01 grone orm (snake clone)/movement.lua
+++ game carts/01 grone orm (snake clone)/movement.lua
@@ -37,10 +37,45 @@ function check_button_push()
 	update_next_dir(dir)
 end
 
+
 -- Move snake and update variable "just_moved" to TRUE
 function move()
 	update_snake()
 	just_moved = true
 end
 
-// LAST_LINE_OF_MOVEMENT_LUA
+
+function check_if_safe(new_dir, x, y)
+	if (new_dir == 0) then
+		x += scale * (-step) -- LEFT
+	elseif (new_dir == 1) then
+		x += scale * (step) -- RIGHT
+	elseif (new_dir == 2) then
+		y += scale * (-step) -- UP
+	elseif (new_dir == 3) then
+		y += scale * (step) -- DOWN
+	end
+	
+	-- Check bounds
+	if x < min or x >= max or y < min or y >= max then
+		return false
+	end
+
+	local tail_x = body.x[body.segments]
+	local tail_y = body.y[body.segments]
+	local will_grow = (x == fruit.x and y == fruit.y)
+	
+	-- Current tail tile will move away unless we grow by eating
+	if (not will_grow) and (x == tail_x and y == tail_y) then
+		return true
+	end
+
+	-- Current tile != tail tile, so run regular check
+	if is_occupied(empty_cells, x, y) then
+		return false
+	else
+		return true
+	end
+end
+
+-- LAST_LINE_OF_MOVEMENT_LUA
