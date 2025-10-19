e0d6e6cb7eadba8a0f380370dfd05546c5adf00a
Kardo Rostam <kardo.rostam@gmail.com>
2025-10-20 01:38:37 +0200

    Checking and designing formatting for top player score

--- game carts/01 grone orm (snake clone)/ui.lua
+++ game carts/01 grone orm (snake clone)/ui.lua
@@ -12,10 +12,19 @@ function print_ui()
   print_centered("< press 🅾️ or \'z\' to retry >", 64, 56, 6)
   --print_centered("adjust starting speed",64,88,5)
 
+  -- Letter (with space) = 4 pixels
+  local first = 20
   print_centered("hiscore", 64, 80, 137)
-  print_centered("1st "..hiscore[1], 64, 88, 137)
+  print("1st ", first, 85, 137)
+  --print("88     88", first+12, 85, 8) -- space check
+  print(hiscore[1], first+28, 85, 137)
+  print("gelaflax", first+48, 85, 137) -- space check
+
   print_centered("2nd "..hiscore[2], 64, 96, 137)
   print_centered("3rd "..hiscore[3], 64, 104, 137)
+end
+
+function print_score()
 end
 
 -- LAST_LINE_OF_UI_LUA
